b5001b911e70c409afdd6a3a9784e513971797bc
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2012-05-07 09:18:07 +0200

    initial commit

---
+++ lib/Catmandu/OAI.pm
@@ -0,0 +1,40 @@
+package Catmandu::OAI;
+
+=head1 NAME
+
+Catmandu::OAI - Catmandu modules for working with OAI repositories
+
+=head1 VERSION
+
+Version 0.1
+
+=cut
+
+our $VERSION = '0.1';
+
+=head1 MODULES
+
+=over
+
+=item * L<Catmandu::Importer::OAI>
+
+=back
+
+=head1 AUTHOR
+
+Nicolas Steenlant, C<< <nicolas.steenlant at ugent.be> >>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 Ghent University Library
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
+1;
+
