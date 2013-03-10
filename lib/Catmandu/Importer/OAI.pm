d899f19a3f2c9961304f0d6a8897b25da947b8e2
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2013-03-10 09:19:22 +0100

    Adding support for broken OAI-PMH repositories. Adding support for metadataPrefix, from, until. Adding support for metadata handler

--- lib/Catmandu/Importer/OAI.pm
+++ lib/Catmandu/Importer/OAI.pm
@@ -1,54 +1,92 @@
+package Catmandu::Importer::OAI::DC;
+use Moo;
+
+has dom => (is => 'ro');
+
+sub parse {
+    my ($self) = @_;
+    
+    return undef unless $self->dom;
+    
+    my $rec = {};
+    
+    for ($self->dom->findnodes("./*")) {
+        my $name  = $_->localName;
+        my $value = $_->textContent;
+        push(@{$rec->{$name}}, $value);
+    }
+    
+    $rec;
+}
+
 package Catmandu::Importer::OAI;
 
 use Catmandu::Sane;
 use Moo;
-use Net::OAI::Harvester;
-
-my $OAI_DC_ELEMENTS = [qw(
-    title
-    creator
-    subject
-    description
-    publisher
-    contributor
-    date
-    type
-    format
-    identifier
-    source
-    language
-    relation
-    coverage
-    rights
-)];
+use HTTP::OAI;
+use Data::Dumper;
 
 with 'Catmandu::Importer';
 
-has url => (is => 'ro', required => 1);
-has set => (is => 'ro');
-has oai => (is => 'ro', lazy => 1, builder => '_build_oai');
+has url     => (is => 'ro', required => 1);
+has metadataPrefix => (is => 'ro' , default => sub { "oai_dc" });
+has handler => (is => 'ro' , default => sub { "Catmandu::Importer::OAI::DC"} );
+has set     => (is => 'ro');
+has from    => (is => 'ro');
+has until   => (is => 'ro');
+has oai     => (is => 'ro', lazy => 1, builder => '_build_oai');
 
 sub _build_oai {
-    Net::OAI::Harvester->new(baseURL => $_[0]->url);
+    my ($self) = @_;
+    HTTP::OAI::Harvester->new(baseURL => $_[0]->url, resume => 1);
 }
 
 sub _map_record {
     my ($self, $rec) = @_;
-    my $data = {_id => $rec->header->identifier};
-    for my $key (@$OAI_DC_ELEMENTS) {
-        my $val = $rec->metadata->{$key};
-        $data->{$key} = $val if @$val;
+    
+    my $sets       = [ $rec->header->setSpec ];
+    my $identifier = $rec->identifier;
+    my $datestamp  = $rec->datestamp;
+    my $status     = $rec->status // ""; 
+    my $dom        = $rec->metadata ? $rec->metadata->dom->nonBlankChildNodes->[0]->nonBlankChildNodes->[0] : undef;
+    my $metadata   = $dom ? $dom->toString : "";
+    my $about      = [];
+    
+    for ($rec->about) {
+        push(@$about , $_->dom->nonBlankChildNodes->[0]->nonBlankChildNodes->[0]->toString);
     }
+    
+    my $handler = Catmandu::Importer::OAI::DC->new(dom => $dom);
+    my $values  = $handler->parse // {};
+
+    my $data = { 
+        _id => $identifier ,
+        _identifier => $identifier ,
+        _datestamp  => $datestamp ,
+        _status     => $status ,
+        _metadata   => $metadata ,
+        _setSpec    => $sets ,
+        _about      => $about ,
+        %$values
+    };
+        
     $data;
 }
 
 sub generator {
     my ($self) = @_;
     sub {
-        state $res = $self->set
-            ? $self->oai->listAllRecords(metadataPrefix => 'oai_dc', set => $self->set)
-            : $self->oai->listAllRecords(metadataPrefix => 'oai_dc');
-        return if $res->errorCode;
+        state $res = $self->oai->ListRecords(
+                                metadataPrefix => $self->metadataPrefix, 
+                                set => $self->set , 
+                                from => $self->from , 
+                                until => $self->until ,
+                    );
+        
+        if ($res->is_error) {
+            warn $res->message;
+            return;
+        }
         if (my $rec = $res->next) {
             return $self->_map_record($rec);
         }
@@ -64,7 +102,12 @@ Catmandu::Importer::OAI - Package that imports OAI-PMH feeds
 
     use Catmandu::Importer::OAI;
 
-    my $importer = Catmandu::Importer::OAI->new(url => "...", set => "..." );
+    my $importer = Catmandu::Importer::OAI->new(
+                    url => "...", 
+                    metadataPrefix => "..." , 
+                    from => "..." , 
+                    until => "..." , 
+                    set => "..." );
 
     my $n = $importer->each(sub {
         my $hashref = $_[0];
